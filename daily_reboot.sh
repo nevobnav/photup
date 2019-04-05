676f2d6685469c3d57a1c3c6ed9b35ceb601f5ec
kmvermeer <kazvermeer@gmail.com>
2019-04-05 09:43:29 +0200

    Upgraded daily_reboot.sh to include date and time

--- daily_reboot.sh
+++ daily_reboot.sh
@@ -2,11 +2,12 @@
 #This script fetches the current state of the photup service. If its not running, then the 
 # system does a reboot. This is called as a cronjob to occur daily at 6am.
 
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
 if [ "$(systemctl show -p SubState --value photup)" != "running" ]; then
-    echo "Photup is not running - moving forward and rebooting system"
+    echo "$DATE - Photup is not running - moving forward and rebooting system"
     /sbin/reboot
 else
-    echo "Photup is still running - not rebooting this time. Try again tomorrow!"
+    echo "$DATE - Photup is still running - not rebooting this time. Try again tomorrow!"
 fi
 
 
